409161540ef0204594fda4b1e39607139197a920
Luis Alfredo da Silva <luis.adasilvaf@gmail.com>
2016-06-18 01:27:11 -0400

    adding type identification and person field

--- static/src/js/main.js
+++ static/src/js/main.js
@@ -4,13 +4,21 @@
     var module = require('point_of_sale.models');
     var models = module.PosModel.prototype.models;
 
+    var partner_fields = ['x_name1',
+                          'x_name2',
+                          'x_lastname1',
+                          'x_lastname2',
+                          'is_company',
+                          'personType',
+                          'doctype',
+                          'xidentification'];
+
     for(var i = 0; i < models.length; i++) {
         if(models[i].model == 'res.partner') {
-            models[i].fields.push('x_name1');
-            models[i].fields.push('x_name2');
-            models[i].fields.push('x_lastname1');
-            models[i].fields.push('x_lastname2');
-            models[i].fields.push('is_company');
+            var model = models[i];
+            for(var j = 0; j < partner_fields.length; j++){
+                model.fields.push(partner_fields[j]);
+            }
         }
     }
 
