26940403a69958418508f56a353d1de4c597939d
Luis Alfredo da Silva <luis.adasilvaf@gmail.com>
2016-06-22 20:35:12 -0400

    hiding partner names when it's a company

--- static/src/js/main.js
+++ static/src/js/main.js
@@ -1,26 +1,51 @@
- odoo.define('l10n_co_point_of_sale', function(require) {
-    "use strict";
+ odoo.define('l10n_co_point_of_sale.main', function(require) {
+"use strict";
 
-    var module = require('point_of_sale.models');
-    var models = module.PosModel.prototype.models;
+var core = require('web.core');
+var module = require('point_of_sale.models');
+var screens = require('point_of_sale.screens');
+var _t = core._t;
 
-    var partner_fields = ['x_name1',
-                          'x_name2',
-                          'x_lastname1',
-                          'x_lastname2',
-                          'is_company',
-                          'personType',
-                          'doctype',
-                          'xidentification'];
+var models = module.PosModel.prototype.models;
+var partner_fields = ['x_name1',
+                      'x_name2',
+                      'x_lastname1',
+                      'x_lastname2',
+                      'is_company',
+                      'personType',
+                      'doctype',
+                      'xidentification'];
 
+var set_fields_to_model = function(fields, models) {
     for(var i = 0; i < models.length; i++) {
         if(models[i].model == 'res.partner') {
             var model = models[i];
-            for(var j = 0; j < partner_fields.length; j++){
-                model.fields.push(partner_fields[j]);
+            for(var j = 0; j < fields.length; j++){
+                model.fields.push(fields[j]);
             }
         }
     }
+}
+set_fields_to_model(partner_fields, models);
 
-    console.log("se registró el módulo");
- });
+// extending client screen behavior
+screens.ClientListScreenWidget.include({
+
+    is_company_click_handler: function(event, $el) {
+        if($el.prop("checked") == true){
+            $('.partner-names').hide();
+        } else {
+            $('.partner-names').show();
+        }
+    },
+
+    edit_client_details: function(partner) {
+        var self = this;
+        this._super(partner);
+        this.$('.client-is-company').click(function(event){
+            self.is_company_click_handler(event, $(this));
+        });
+    },
+});
+
+});
