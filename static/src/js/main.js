b541b0100b4cbf7c1b635a16c1b127d07ed30046
Luis Alfredo da Silva <luis.adasilvaf@gmail.com>
2016-06-28 14:31:12 -0400

    usando los campos individuales de nombres cuando no sea empresa

--- static/src/js/main.js
+++ static/src/js/main.js
@@ -33,21 +33,56 @@ set_fields_to_model(partner_fields, models);
 // extending client screen behavior
 screens.ClientListScreenWidget.include({
 
-    is_company_click_handler: function(event, $el) {
-        if($el.prop("checked") == true){
+    is_company_click_handler: function($el) {
+        var is_company = $(".client-is-company");
+        var name = $(".client-name");
+
+        name.val("");
+        if(is_company.is(":checked")) {
+            name.removeAttr("disabled");
+            $(".client-first-name").val("");
+            $(".client-second-name").val("");
+            $(".client-first-lastname").val("");
+            $(".client-second-lastname").val("");
             $('.partner-names').hide();
         } else {
+            name.attr("disabled", "disabled");
             $('.partner-names').show();
         }
+
+    },
+
+    _concat_names: function(event, $el) {
+        var first_name = $el.find(".client-first-name").val();
+        var second_name = $el.find(".client-second-name").val();
+        var first_lastname = $el.find(".client-first-lastname").val();
+        var second_lastname = $el.find(".client-second-lastname").val();
+
+        $el.find(".client-name").val(first_name + " " + second_name + " " +
+                              first_lastname+ " " + second_lastname);
+    },
+
+    setup_res_partner_logic: function() {
+        this.is_company_click_handler($(this));
+        var self = this;
+
+        var names = [".client-first-name",
+                     ".client-second-name",
+                     ".client-first-lastname",
+                     ".client-second-lastname"];
+
+        $(names.join(", ")).keyup(function(event) {
+              console.log("test");
+              self._concat_names(event, $(".client-details"));
+        });
+        this.$('.client-is-company').click(function(event){
+            self.is_company_click_handler($(this));
+        });
     },
 
     display_client_details: function(visibility,partner,clickpos) {
-        var self = this;
         this._super(visibility,partner,clickpos);
-        this.$('.client-is-company').click(function(event){
-            self.is_company_click_handler(event, $(this));
-        });
-
+        this.setup_res_partner_logic();
     },
 });
 
