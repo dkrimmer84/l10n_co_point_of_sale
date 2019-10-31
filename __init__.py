d1109bfb310011a4672a94d0309729e00ab14fd1
root <root@odoo12ayer.c.aerial-reality-126814.internal>
2019-10-31 13:03:31 +0000

    prep 12.0

--- __init__.py
+++ __init__.py
@@ -1,7 +1,6 @@
-# -*- coding: utf-8 -*-
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
 # Copyright (C) Luis Alfredo da Silva (Plastinorte S.A.S).
 # Author        Luis Alfredo da Silva, luis.adasilvaf@gmail.com
 # Co-Authors    Dominic Krimmer, dominic.krimmer@gmail.com
-import models
+from . import models
